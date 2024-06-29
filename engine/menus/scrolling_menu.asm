c16543009c5ca063793b4aa0993db8d902683279
vulcandth <vulcandth@gmail.com>
2024-06-28 21:25:43 -0500

    Bugfix: main items pocket sorting
    resolves #26

--- engine/menus/scrolling_menu.asm
+++ engine/menus/scrolling_menu.asm
@@ -491,9 +491,6 @@ ScrollingMenu_GetListItemCoordAndFunctionArgs:
 	ld a, [wMenuData_ScrollingMenuItemFormat]
 	cp SCROLLINGMENU_ITEMS_NORMAL
 	jr z, .got_spacing
-	cp SCROLLINGMENU_ITEMS_QUANTITY
-	jr z, .pointless_jump
-.pointless_jump
 	add hl, de
 .got_spacing
 	add hl, de
@@ -513,9 +510,6 @@ ScrollingMenu_GetListItemCoordAndFunctionArgs16bit:
 	ld a, [wMenuData_ScrollingMenuItemFormat]
 	cp SCROLLINGMENU_ITEMS_NORMAL
 	jr z, .got_spacing
-	cp SCROLLINGMENU_ITEMS_QUANTITY
-	jr z, .pointless_jump
-.pointless_jump
 	add hl, de
 .got_spacing
 	add hl, de
