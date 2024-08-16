6b9c373c219221b5becf52f3b201ea5a54e35e17
8bitZeta <devin94@protonmail.com>
2024-08-15 22:30:08 -0500

    Optimize Meteor Mash Animation

--- data/moves/animations_gen3.asm
+++ data/moves/animations_gen3.asm
@@ -1443,12 +1443,11 @@ BattleAnim_MeteorMash:
 	anim_3gfx BATTLE_ANIM_GFX_STARS, BATTLE_ANIM_GFX_HIT, BATTLE_ANIM_GFX_WATER
 	anim_bgp $1b
 	anim_sound 0, 1, SFX_MORNING_SUN
+.loop
 	anim_obj BATTLE_ANIM_OBJ_RAIN, 88, 0, $2
 	anim_wait 8
-	anim_obj BATTLE_ANIM_OBJ_RAIN, 88, 0, $2
-	anim_wait 8
-	anim_obj BATTLE_ANIM_OBJ_RAIN, 88, 0, $2
-	anim_wait 64
+	anim_loop 3, .loop
+	anim_wait 56
 	anim_clearobjs
 	anim_bgeffect BATTLE_BG_EFFECT_CYCLE_OBPALS_GRAY_AND_YELLOW, $0, $2, $0
 	anim_bgeffect BATTLE_BG_EFFECT_SHAKE_SCREEN_X, $28, $2, $0
@@ -1456,18 +1455,15 @@ BattleAnim_MeteorMash:
 	anim_obj BATTLE_ANIM_OBJ_LONG_PUNCH, 136, 56, $0
 	anim_wait 3
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $28
-	anim_wait 3
-	anim_sound 0, 1, SFX_EGG_BOMB
+	anim_call BattleAnimSub_MeteorMash1
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $5c
 	anim_wait 3
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $10
-	anim_wait 3
-	anim_sound 0, 1, SFX_EGG_BOMB
+	anim_call BattleAnimSub_MeteorMash1
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $e8
 	anim_wait 3
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $9c
-	anim_wait 3
-	anim_sound 0, 1, SFX_EGG_BOMB
+	anim_call BattleAnimSub_MeteorMash1
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $d0
 	anim_wait 3
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $1c
@@ -1478,6 +1474,11 @@ BattleAnim_MeteorMash:
 	anim_wait 3
 	anim_obj BATTLE_ANIM_OBJ_STAR_BURST, 136, 56, $90
 	anim_wait 32
+	anim_ret
+
+BattleAnimSub_MeteorMash1:
+	anim_wait 3
+	anim_sound 0, 1, SFX_EGG_BOMB
 	anim_ret
 
 BattleAnim_Astonish:
--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -140,7 +140,7 @@ if DEF(_DEBUG)
 	; givepokemove WATERFALL,  wPartyMon3, 3
 	; misc pokemon for testing
 	givepoke SNORLAX, 75
-	givepokemove BLAST_BURN,  wPartyMon1, 0
+	givepokemove METEOR_MASH,  wPartyMon1, 0
 	givepokemove BLAZE_KICK,  wPartyMon1, 1
 	givepokemove PRESENT,  wPartyMon1, 2
 	givepokemove MEGA_DRAIN,  wPartyMon1, 3
