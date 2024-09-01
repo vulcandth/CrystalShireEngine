760df2a763bc6298eb229da0ab6f69b5703efb38
vulcandth <vulcandth@gmail.com>
2024-09-01 13:57:10 -0500

    Update debug radio

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -60,6 +60,21 @@ if DEF(_DEBUG)
 	; useful items
 	giveitem MAX_REPEL, 99
 	giveitem MASTER_BALL, 99
+	giveitem BICYCLE
+	giveitem GOOD_ROD
+	giveitem THUNDERSTONE, 10
+	giveitem FIRE_STONE, 10
+	giveitem WATER_STONE, 10
+	giveitem LEAF_STONE, 10
+	giveitem MOON_STONE, 10
+	giveitem BERRY, 10
+	giveitem GOLD_BERRY, 10
+	giveitem MINT_BERRY, 10
+	giveitem MYSTERYBERRY, 10
+	giveitem QUICK_CLAW
+	giveitem KINGS_ROCK
+	giveitem AMULET_COIN
+	giveitem SCOPE_LENS
 	; all badges
 	setflag ENGINE_ZEPHYRBADGE
 	setflag ENGINE_HIVEBADGE
@@ -125,7 +140,7 @@ if DEF(_DEBUG)
 	givepoke MEWTWO, 100, BRIGHTPOWDER
 	; hm slaves
 	givepoke MEW, 100, LEFTOVERS
-	givepoke MEW, 100, LEFTOVERS
+	givepoke LANTURN, 100, LEFTOVERS
 	givepokemove FLY,        wPartyMon2, 0
 	givepokemove SURF,       wPartyMon2, 1
 	givepokemove STRENGTH,   wPartyMon2, 2
@@ -135,7 +150,7 @@ if DEF(_DEBUG)
 	givepokemove HEADBUTT,   wPartyMon3, 2
 	givepokemove WATERFALL,  wPartyMon3, 3
 	; misc pokemon for testing
-	givepoke PIKACHU, 50
+	givepoke AMPHAROS, 50
 	givepoke GENGAR, 50
 	givepoke DITTO, 50
 	; intro events
