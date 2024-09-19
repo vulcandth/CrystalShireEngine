d124427b2053c34dc23ea14ecd81b465e1be237d
8bitZeta <devin94@protonmail.com>
2024-09-19 00:47:15 -0500

    Add `MultiplyAndDivide` from Polished Crystal
    Co-Authored-By: Sylvie <35663410+Rangi42@users.noreply.github.com>
    Co-Authored-By: Fredrik Ljungdahl <fredde1994@gmail.com>

--- home/math.asm
+++ home/math.asm
@@ -43,3 +43,21 @@ Divide::
 	push bc
 	homecall _Divide
 	jmp PopBCDEHL
+
+MultiplyAndDivide::
+; a = $xy: multiply multiplicands by x, then divide by y
+; Used for damage modifiers, catch rate modifiers, etc.
+	push bc
+	ld b, a
+	swap a
+	and $f
+	ld c, LOW(hMultiplier)
+	ldh [c], a
+	call Multiply
+	ld a, b
+	and $f
+	ldh [c], a
+	ld b, 4
+	call Divide
+	pop bc
+	ret
