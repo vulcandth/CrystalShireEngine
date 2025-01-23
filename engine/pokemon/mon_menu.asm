0ae2f7b3aa881cdd5df9feaf23b5ebdeb5c5095c
vulcandth <vulcandth@gmail.com>
2025-01-22 21:59:16 -0600

    Bugfix: Fix giving an item to mon via newbox

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -273,10 +273,12 @@ _GetItemToGive:
 	jr .loop
 
 PCGiveItem:
+	ld hl, wItemFlags
+	set IN_BAG_F, [hl]
 	call DepositSellInitPackBuffers
 .loop
 	call _GetItemToGive
-	ret z
+	jr z, .done
 
 	; Ensure that we aren't trying to give Mail to a Pokémon in storage.
 	ld a, [wCurItem]
@@ -320,8 +322,13 @@ PCGiveItem:
 	ld a, [wCurItem]
 	ld d, a
 	farcall ItemIsMail
-	ret nc
-	jmp ComposeMailMessage
+	jr nc, .done
+	call ComposeMailMessage
+
+.done
+	ld hl, wItemFlags
+	res IN_BAG_F, [hl]
+	ret
 
 TryGiveItemToPartymon:
 	call SpeechTextbox
